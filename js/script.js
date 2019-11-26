e2f9da477b28d5e4cea809cbdc46a4855100db36
Lisa Drozdovskaya <lisadrozdovskaya@gmail.com>
2019-11-26 09:04:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,8 +128,8 @@ $('.one-time').slick({
     
 
 // ---------line bar--------
-$('.probar').waypoint(function (bar){
-    if (bar == 'down') {
+$('.probar').waypoint(function (a){
+    if (a == 'down') {
         $('.probar').addClass('js-probar');
     } else {
         $('.probar').removeClass('js-probar');
